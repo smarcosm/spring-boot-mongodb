0f255926d77e9b26594579a16527390c7fe1457b
smarcosm <sebastiaomarcos49@gmail.com>
2022-11-17 19:03:25 -0300

    Consulta simples com @Query

--- src/main/java/com/smarcosm/springmongo/repository/PostRepository.java
+++ src/main/java/com/smarcosm/springmongo/repository/PostRepository.java
@@ -3,11 +3,16 @@ package com.smarcosm.springmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.smarcosm.springmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/smarcosm/springmongo/services/PostService.java
+++ src/main/java/com/smarcosm/springmongo/services/PostService.java
@@ -25,7 +25,7 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	public List<Post> findByTitle(String text){
-		return repo.findByTitleContainingIgnoreCase(text);
+		return repo.searchTitle(text);
 	}
 	
 }
