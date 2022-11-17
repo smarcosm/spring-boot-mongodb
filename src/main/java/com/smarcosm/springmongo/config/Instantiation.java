c079ca6c06c57395f496d319c9b109735473670b
smarcosm <sebastiaomarcos49@gmail.com>
2022-11-17 16:41:04 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/smarcosm/springmongo/config/Instantiation.java
+++ src/main/java/com/smarcosm/springmongo/config/Instantiation.java
@@ -10,36 +10,38 @@ import org.springframework.context.annotation.Configuration;
 
 import com.smarcosm.springmongo.domain.Post;
 import com.smarcosm.springmongo.domain.User;
+import com.smarcosm.springmongo.dto.AuthorDTO;
 import com.smarcosm.springmongo.repository.PostRepository;
 import com.smarcosm.springmongo.repository.UserRepository;
 
 @Configuration
-public class Instantiation implements CommandLineRunner{
-	
+public class Instantiation implements CommandLineRunner {
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
+
 		userRepository.deleteAll();
 		postRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2022"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2022"), "Bom dia", "Acordei feliz hoje!", maria);
-		
-		
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2022"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
+				new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2022"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
