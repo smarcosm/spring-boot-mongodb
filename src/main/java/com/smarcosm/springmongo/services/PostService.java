f0a99ab4b7cc4d3a3b38113228ce5cbf9cbe4361
smarcosm <sebastiaomarcos49@gmail.com>
2022-11-17 17:34:01 -0300

    Obtendo um post por id

---
+++ src/main/java/com/smarcosm/springmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.smarcosm.springmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.smarcosm.springmongo.domain.Post;
+import com.smarcosm.springmongo.repository.PostRepository;
+import com.smarcosm.springmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
