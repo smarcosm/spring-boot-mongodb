e99cc5e288ac011e4f427090cc653f2722da8e0c
smarcosm <sebastiaomarcos49@gmail.com>
2022-11-17 17:17:00 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/smarcosm/springmongo/resources/UserResource.java
+++ src/main/java/com/smarcosm/springmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.smarcosm.springmongo.domain.Post;
 import com.smarcosm.springmongo.domain.User;
 import com.smarcosm.springmongo.dto.UserDTO;
 import com.smarcosm.springmongo.services.UserService;
@@ -57,5 +58,10 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
