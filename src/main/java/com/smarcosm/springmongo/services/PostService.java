f1e989556a82892e6eb4a39475293b9d33e4428e
smarcosm <sebastiaomarcos49@gmail.com>
2022-11-17 18:47:23 -0300

    Consulta simples com query methods

--- src/main/java/com/smarcosm/springmongo/services/PostService.java
+++ src/main/java/com/smarcosm/springmongo/services/PostService.java
@@ -24,5 +24,8 @@ public class PostService {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 	
 }
